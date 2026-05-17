ac7798aa7fd8b9cf936752b6895f8f45a8644089
s1mcer <gabitacristea04@gmail.com>
2026-05-17 20:47:09 +0300

    fix sonarqube issues

--- smart-city-app/src/main/java/com/smartcity/parking/dto/request/ParkingPaymentRequest.java
+++ smart-city-app/src/main/java/com/smartcity/parking/dto/request/ParkingPaymentRequest.java
@@ -4,7 +4,6 @@ import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
-import lombok.Data;
 
 @Builder
 public record ParkingPaymentRequest (
